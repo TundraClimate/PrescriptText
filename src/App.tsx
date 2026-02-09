27b57fed61f6a85bdd394c407194106d174a65c9
TundraClimate <tundra.tundere@gmail.com>
2026-02-10 01:08:33 +0900

    ✨feat: Add view

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { PxCanvas } from "./PxCanvas";
 import { Scramble } from "./Scramble";
 
 export default () => {
     return (
         <div class="content">
+            <input />
             <header>
                 <Scramble trigger={() => true} scale={8} maxPerRoll={5}>
                     _Make Your Prescript._
                 </Scramble>
             </header>
+            <div class="view">
+                <PxCanvas scale={8}>Hello</PxCanvas>
+            </div>
+            <div class="nav"></div>
             <footer>
                 <Scramble trigger={() => true} scale={3} maxPerRoll={10} wait={3}>
                     DEVELOP:TundraClimate
