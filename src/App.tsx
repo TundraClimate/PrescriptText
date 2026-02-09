bca7727a8fbd91302dcd547fb05385692172c104
TundraClimate <tundra.tundere@gmail.com>
2026-02-10 01:57:57 +0900

    ✨feat: Add input method

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import { PxCanvas } from "./PxCanvas";
 import { Scramble } from "./Scramble";
 import { ButtonScramble } from "./ButtonCanvas";
+import { onMount } from "solid-js";
 
 export default () => {
+    const focusInput = () => {
+        document.getElementById("hidden-input")!.focus();
+    };
+
+    onMount(focusInput);
+
     return (
         <div class="content">
-            <input />
+            <input id="hidden-input" onInput={(input) => console.log(input.data)} />
             <header>
                 <Scramble scale={8} maxPerRoll={5}>
                     _Make Your Prescript._
                 </Scramble>
             </header>
             <div class="view">
-                <PxCanvas scale={8}>Hello</PxCanvas>
+                <PxCanvas scale={8} onClick={focusInput}>
+                    Hello
+                </PxCanvas>
             </div>
             <div class="nav"></div>
             <footer>
