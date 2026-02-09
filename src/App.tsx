a0cd08630caaf12f8e27722b3ba3ce272bb27035
TundraClimate <tundra.tundere@gmail.com>
2026-02-10 00:34:27 +0900

    ✨feat: Add header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,18 @@
-import { PxCanvas } from "./PxCanvas";
 import { Scramble } from "./Scramble";
-import { createSignal } from "solid-js";
 
 export default () => {
-    const [trigger, setTrigger] = createSignal(true);
-
     return (
-        <div>
-            <Scramble trigger={trigger} maxPerRoll={20} wait={1}>
-                Sacrifice is the easy path
-            </Scramble>
-            <br />
-            <Scramble trigger={trigger} maxPerRoll={20} wait={1}>
-                And S is not for sayonara
-            </Scramble>
-            <br />
-            <Scramble trigger={trigger} maxPerRoll={20} wait={1}>
-                Will you forgive me at last?]
-            </Scramble>
-            <br />
-            <button onClick={() => setTrigger((b) => !b)} />
-            <br />
-            <PxCanvas>0123456789</PxCanvas>
-            <br />
-            <PxCanvas>ABCDEFG</PxCanvas>
-            <br />
-            <PxCanvas>HIJKLMN</PxCanvas>
-            <br />
-            <PxCanvas>OPQRSTU</PxCanvas>
-            <br />
-            <PxCanvas>VWXYZ</PxCanvas>
-            <br />
-            <PxCanvas>abcdefg</PxCanvas>
-            <br />
-            <PxCanvas>hijklmn</PxCanvas>
-            <br />
-            <PxCanvas>opqrstu</PxCanvas>
-            <br />
-            <PxCanvas>vwxyz</PxCanvas>
-            <br />
-            <PxCanvas>,._</PxCanvas>
+        <div class="content">
+            <header>
+                <Scramble trigger={() => true} scale={8} maxPerRoll={5}>
+                    _Make Your Prescript._
+                </Scramble>
+            </header>
+            <footer>
+                <Scramble trigger={() => true} scale={3} maxPerRoll={10} wait={3}>
+                    DEVELOP:TundraClimate
+                </Scramble>
+            </footer>
         </div>
     );
 };
